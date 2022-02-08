940a6f1a3e4d5f95ad54e4b18aa6a33463992205
FabioSntos <Fabio_santos_2001@outlook.com>
2022-02-07 21:38:58 -0300

    fix

--- src/modules/cars/useCases/createSpecification/CreateSpecificationService.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationService.ts
@@ -1,4 +1,4 @@
-import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
 	name: string;
