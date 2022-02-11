552f0b002ec65b7a1a3925f07cab4bc7af6712ae
FabioSntos <Fabio_santos_2001@outlook.com>
2022-02-10 22:34:13 -0300

    upload de arquivos com multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (req, res) => {
 	return createCategoryController.handle(req, res);
@@ -12,4 +15,11 @@ categoriesRoutes.post("/", (req, res) => {
 categoriesRoutes.get("/", (req, res) =>
 	listCategoriesController.handle(req, res)
 );
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+	const { file } = req;
+	console.log(file);
+
+	return res.send();
+});
 export { categoriesRoutes };
