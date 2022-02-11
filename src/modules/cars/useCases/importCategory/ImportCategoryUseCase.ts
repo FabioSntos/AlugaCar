ffc8d7eb62b687c15afc416f2dc4e74e6651e721
FabioSntos <Fabio_santos_2001@outlook.com>
2022-02-11 15:50:06 -0300

    inserindo os dados de upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -35,6 +35,16 @@ export class ImportCategoryUseCase {
 	}
 	async execute(file: Express.Multer.File): Promise<void> {
 		const categories = await this.loadCategories(file);
-		console.log(categories);
+		categories.map(async category => {
+			const { name, description } = category;
+			const existCategory = this.categoriesRepository.findByName(name);
+
+			if (!existCategory) {
+				this.categoriesRepository.create({
+					name,
+					description,
+				});
+			}
+		});
 	}
 }
