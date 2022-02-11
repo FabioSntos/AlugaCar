96ec10b4313acf78828b308eba65dc8e03cbf7d5
FabioSntos <Fabio_santos_2001@outlook.com>
2022-02-10 23:10:45 -0300

    stream com csvParse

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -5,7 +5,9 @@ export class ImportCategoryController {
 	constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
 	handle(req: Request, res: Response): Response {
 		const { file } = req;
+
 		this.importCategoryUseCase.execute(file);
+
 		return res.send();
 	}
 }
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,16 @@
+import fs from "fs";
+import { parse } from "csv-parse";
+
 export class ImportCategoryUseCase {
-	execute(file: any) {
-		console.log(file);
-		return;
+	execute(file?: Express.Multer.File) {
+		// @ts-ignore: Unreachable code error
+		const stream = fs.createReadStream(file.path);
+
+		const parseFile = parse();
+
+		stream.pipe(parseFile);
+		parseFile.on("data", async line => {
+			console.log(line);
+		});
 	}
 }
