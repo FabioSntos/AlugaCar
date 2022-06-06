34c2d61fbd87b9bebc50a84c110c885991b92401
FabioSntos <fabio_santos_2001@outlook.com>
2022-06-06 15:10:01 -0300

    config para depuração

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -9,7 +9,9 @@ export class CreateCategoryUseCase {
 	constructor(private categoriesRepository: ICategoriesRepository) {}
 
 	async execute({ name, description }: IRequest): Promise<void> {
-		const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
+		const categoryAlreadyExists = await this.categoriesRepository.findByName(
+			name
+		);
 
 		if (categoryAlreadyExists) {
 			throw new Error("Category already exists");
