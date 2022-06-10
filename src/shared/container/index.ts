7254935a2bb8c39cdaaae1bee010a40bd0048d80
FabioSntos <fabio_santos_2001@outlook.com>
2022-06-09 21:15:51 -0300

    fix injeção de dependencias de specifications

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,6 +12,6 @@ container.registerSingleton<ICategoriesRepository>(
 );
 
 container.registerSingleton<ISpecificationsRepository>(
-	"SpecificationRepository",
+	"SpecificationsRepository",
 	SpecificationsRepository
 );
